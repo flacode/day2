26cfb17e75e7c50cbbe3015fca3db933f363eca1
flacode <flavianshemerirwe@gmail.com>
2015-10-30 00:31:48 +0300

    [my solution]

---
+++ day2.py
@@ -0,0 +1,13 @@
+T=int(raw_input())
+N=[]
+count=1
+while count<=T:
+    N.append(int(raw_input()))
+    count=count+1
+for i in N:
+    sum=0
+    for j in range(1, i):
+        if j%3==0 or j%5==0:
+            sum=sum+j
+    print sum
+            
